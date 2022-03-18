03361fed934a139db15a4a80b696c7b8e7d885fc
nipun <nipun.s@solutelabs.com>
2022-03-19 01:00:46 +0530

    New Content

--- DSA Algo Problem Solving/algos_programs_1(cherry_picked)/tree_traversal.py
+++ DSA Algo Problem Solving/algos_programs_1(cherry_picked)/tree_traversal.py
@@ -21,3 +21,38 @@ def postorderTraversal(root: Optional[TreeNode]) -> List[int]:
 
     dfs(root)
     return post
+
+def inorderTraversal1(root: Optional[TreeNode]) -> List[int]:
+    '''Recursively'''
+    res = []
+    def inorder(node):
+        if not node:
+            return
+        # 1. Left Child 
+        inorder(node.left)
+        # 2. Curr Node 
+        res.append(node.val)
+        # 3. Right Child 
+        inorder(node.right)
+    #initiate the traversal
+    inorder(root)
+    return res
+
+def inorderTraversal2(root: Optional[TreeNode]) -> List[int]:
+    '''Stack + Iterative Manner'''
+    res = []
+    curr = root  # pointer to current node
+    stack = []   # stack to keep track of node at upper levels
+
+    while curr or stack:  # till any node exists
+        # 1. Explore the Depth First Left Most
+        while curr:
+            stack.append(curr)
+            curr = curr.left
+        # 2. Add the Top-most elem on stack to {res}
+        top = stack.pop()
+        res.append(top.val)
+        # 3. Explore the Right Part for {top}
+        curr = top.right
+
+    return res
